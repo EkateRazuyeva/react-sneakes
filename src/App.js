20ba963ab48c7e6a0eea310844cec3dc85a55d7f
Ekate Razuyeva <25ky-ky08@mail.ru>
2024-08-28 18:25:02 +0300

    useState, api

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
-import {Card} from "./components/Card";
+import {Card} from "./components/Card/";
 import {Header} from "./components/Header";
 import {Drawer} from "./components/Drawer";
+import {useEffect, useState} from "react";
 
 
 function App() {
+
+    const [items, setItems] = useState([])
+    const [cartItems, setCartItems] = useState([])
+
+    const [cartOpened, setCartOpened] = useState(false)
+
+    useEffect(() => {
+        fetch('https://66cf2f64901aab2484214257.mockapi.io/items').then(res => {
+            return res.json()
+        }).then(json => {
+            setItems(json)
+        })
+    }, []);
+
+    const onAddToCart = (obj) => {
+        setCartItems(prev=>[...prev, obj])
+    }
+
     return (
         <div className="wrapper clear">
-            <Drawer/>
-            <Header/>
+            {cartOpened && <Drawer items={cartItems} onClose={() => setCartOpened(false)}/>}
+            <Header onClickCart={() => setCartOpened(true)}/>
             <div className="content p-40">
                 <div className={'d-flex align-center justify-between mb-40'}>
                     <h1>Все кроссовки</h1>
@@ -16,8 +35,15 @@ function App() {
                         <input placeholder={"Поиск..."}/>
                     </div>
                 </div>
-                <div className={'d-flex'}>
-                    <Card/>
+                <div className={'d-flex flex-wrap'}>
+                    {
+                        items.map((item) => (
+                            <Card onPlus={(obj) => onAddToCart(obj)}
+                                  title={item.title}
+                                  price={item.price}
+                                  imgUrl={item.imgUrl}/>
+                        ))
+                    }
                 </div>
 
             </div>
